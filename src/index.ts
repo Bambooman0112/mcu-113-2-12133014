0ff16cb6177e40e851c2b75dd6c61c934eb94b36
Sky <sky776739@gmail.com>
2025-03-19 17:51:47 +0800

    feat(ts): 利用日期類別變數紀錄產品建構日期

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 let productID = 1;
 console.log("Product Id = ", productID);
+
 const productName = "圖像 Angular 開發入門 第二版";
 console.log("Product Name = ", productName);
 
 let isShow = true;
 console.log("Is Show = ", isShow);
 
-let createDate = undefined;
+let createDate = new Date();
 console.log("Create Date = ", createDate);
