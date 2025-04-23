b42ad5ddb47a3dc87f430916f0da269a8b309fb9
Sky <sky776739@gmail.com>
2025-04-23 15:38:56 +0800

    feat(product): 依產品清單顯示實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductCardListComponent } from './product-card-list/product-card-list.component';
+import { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,47 @@ import { ProductCardListComponent } from './product-card-list/product-card-list.
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  product = [
+    new Product({
+      id: 1,
+      name: '書籍 A',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img2',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 2,
+      name: '書籍 B',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img2',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 3,
+      name: '書籍 C',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img2',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+    new Product({
+      id: 4,
+      name: '書籍 D',
+      authors: '作者甲、作者乙、作者丙',
+      company: '博碩文化',
+      isShow: true,
+      photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img2',
+      createDate: new Date('2025/4/9'),
+      price: 10000,
+    }),
+  ];
+}
