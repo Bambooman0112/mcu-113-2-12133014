d04df17976d1d314c0ef462e0760bfdaef510f52
Sky <sky776739@gmail.com>
2025-05-04 18:37:17 +0800

    feat(product): 實作產品資料分頁

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -3,10 +3,11 @@ import { ProductCardListComponent } from '../product-card-list/product-card-list
 import { Product } from '../models/product';
 import { Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
+import { PaginationComponent } from '../pagination/pagination.component';
 
 @Component({
   selector: 'app-product-page',
-  imports: [ProductCardListComponent],
+  imports: [PaginationComponent, ProductCardListComponent],
   templateUrl: './product-page.component.html',
   styleUrl: './product-page.component.scss',
 })
@@ -14,10 +15,14 @@ export class ProductPageComponent implements OnInit {
   private router = inject(Router);
   private productService = inject(ProductService);
 
+  pageIndex = 1;
+  pageSize = 5;
+  totalCount = 0;
+
   products: Product[] = [];
 
   ngOnInit(): void {
-    this.products = this.productService.getList();
+    this.getProducts();
   }
 
   onEdit(product: Product): void {
@@ -25,5 +30,16 @@ export class ProductPageComponent implements OnInit {
   }
   onView(product: Product): void {
     this.router.navigate(['products', 'view', product.id]);
+  }
+
+  onPageIndexChange(pageIndex: number): void {
+    this.pageIndex = pageIndex;
+    this.getProducts();
+  }
+
+  private getProducts(): void {
+    const { data, count } = this.productService.getList(undefined, this.pageIndex, this.pageSize);
+    this.products = data;
+    this.totalCount = count;
   }
 }
