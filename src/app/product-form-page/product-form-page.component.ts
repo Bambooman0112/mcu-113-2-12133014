7a68354398315e2b4c8e6e38a2f447e73e4d8b49
Sky <sky776739@gmail.com>
2025-05-21 18:08:28 +0800

    feat(product-form): 產品表單名稱、出版社、單價欄位設定

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -3,15 +3,23 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { JsonPipe } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './product-form-page.component.html',
   styleUrl: './product-form-page.component.scss',
 })
 export class ProductFormPageComponent implements OnInit {
   private readonly route = inject(ActivatedRoute);
+
+  form = new FormGroup({
+    id: new FormControl<String | null>(null),
+    name: new FormControl<String | null>(null),
+    company: new FormControl<String | null>(null),
+    price: new FormControl<String | null>(null),
+  });
 
   product!: Product;
 
