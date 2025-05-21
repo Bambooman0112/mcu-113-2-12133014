5c9e14f1889507c51fbf69221a40e5b63776bd45
Sky <sky776739@gmail.com>
2025-05-21 18:29:56 +0800

    feat(product-form): 實作產品作者表單

--- src/app/product-form-page/product-form-page.component.ts
+++ src/app/product-form-page/product-form-page.component.ts
@@ -3,7 +3,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
 import { map } from 'rxjs';
 import { JsonPipe } from '@angular/common';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form-page',
@@ -17,13 +17,23 @@ export class ProductFormPageComponent implements OnInit {
   form = new FormGroup({
     id: new FormControl<String | null>(null),
     name: new FormControl<String | null>(null),
+    authors: new FormArray<FormControl<String | null>>([]),
     company: new FormControl<String | null>(null),
     price: new FormControl<String | null>(null),
   });
 
   product!: Product;
 
+  get authors(): FormArray<FormControl<String | null>> {
+    return this.form.get('authors') as FormArray<FormControl<String | null>>;
+  }
+
   ngOnInit(): void {
     this.route.data.pipe(map(({ product }: Data) => product)).subscribe((product) => (this.product = product));
+  }
+
+  onAddAuthor(): void {
+    const formControl = new FormControl<string | null>(null);
+    this.authors.push(formControl);
   }
 }
